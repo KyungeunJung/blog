467e9f238714aa80ce3ed93158ede974bf9889c1
DESKTOP-2JDVF1K\user <kyungeun08@gmail.com>
2018-06-21 01:41:52 +0900

    no message

---
+++ 21761110_Jung Kyungeun/js/script.js
@@ -0,0 +1,14 @@
+
+function window_onload(){
+
+    setTimeout('go_url()',5000)  // 5초후 go_url() 함수를 호출한다.
+
+ }
+
+ function go_url(){
+
+    location.href="../html/index_tabmenu.html"  // 페이지 이동...
+
+ }
+
+
